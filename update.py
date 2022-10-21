c2d4797c5760a5115520c1266ad49b51ad83f307
hsn1998 <108673037+hsn1998@users.noreply.github.com>
2022-10-21 07:53:57 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,1 @@
-import os
-from time import sleep
-cmd = 'rm -rf KKK'
-os.system(cmd)
-print("done")
-sleep(20)
-cmd = 'git clone https://hsn1998:Fqp!fEvnX!vP_$6@github.com/hsn1998/KKK.git'
-os.system(cmd)
-#https://teamdynamix.umich.edu/TDClient/47/LSAPortal/KB/ArticleDet?ID=1517
+
