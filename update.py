d0bfe3919af81ff87ed95c7ae4fd69ef0b8459c5
hsn1998 <108673037+hsn1998@users.noreply.github.com>
2022-10-21 08:14:58 +0300

    Update update.py

--- update.py
+++ update.py
@@ -5,6 +5,6 @@ cmd = 'rm -rf KKK'
 os.system(cmd)
 print("done")
 sleep(3)
-cmd = 'git clone https://hsn1998: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@github.com/hsn1998/KKK.git'
+cmd = 'git clone https://hsn1998:@github.com/hsn1998/KKK.git'
 os.system(cmd)
 #https://teamdynamix.umich.edu/TDClient/47/LSAPortal/KB/ArticleDet?ID=1517
